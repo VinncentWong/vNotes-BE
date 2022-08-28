f8fbd24806f88269ac50217a62eda4f3a80be26a
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-28 11:29:37 +0700

    init

---
+++ src/main/java/com/demo/VNotesBeApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNotesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VNotesBeApplication.class, args);
+	}
+
+}

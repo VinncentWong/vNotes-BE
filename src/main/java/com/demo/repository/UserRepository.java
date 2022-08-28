d38cd9a71d0d8bd108e91b550439f246101b03a9
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-28 11:46:27 +0700

    add repository annotation

--- src/main/java/com/demo/repository/UserRepository.java
+++ src/main/java/com/demo/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.demo.domain.entity.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{}

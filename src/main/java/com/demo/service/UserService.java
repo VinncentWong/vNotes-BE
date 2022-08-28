9d2c54a04a3785ba97ef80d3a344c339fa8e98b2
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-28 13:24:08 +0700

    add registration service

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -3,8 +3,15 @@ package com.demo.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.dto.LoginDto;
+import com.demo.domain.dto.RegistrationDto;
+import com.demo.domain.entity.Response;
+import com.demo.domain.entity.Role;
+import com.demo.domain.entity.User;
 import com.demo.repository.UserRepository;
 
 @Service
@@ -18,5 +25,14 @@ public class UserService {
 		this.repo = repo;
 	}
 	
+	public ResponseEntity<Response> registerUser(RegistrationDto dto){
+		User user = new User();
+		user.setName(dto.getUsername());
+		user.setPassword(dto.getPassword());
+		user.setEmail(dto.getEmail());
+		user.setRole(Role.USER);
+		User result = repo.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(new Response("success create user", true, result));
+	}
 	
 }

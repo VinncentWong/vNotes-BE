e98cd8b2bc74d7c580d56b667d5e269b24101df3
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-28 14:52:40 +0700

    add rest controller aspect

---
+++ src/main/java/com/demo/aspect/CustomControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.demo.aspect;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.demo.exception.ErrorDetails;
+import com.demo.exception.UserNotFoundException;
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ErrorDetails> userNotFound(){
+		ErrorDetails error = new ErrorDetails();
+		error.setMessage("data user tidak ditemukan! ");
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+	}
+	
+	@ExceptionHandler()
+	public ResponseEntity<ErrorDetails> userNotAuthenticated(){
+		ErrorDetails error = new ErrorDetails();
+		error.setMessage("data user tidak valid, user tidak terautentikasi!");
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+	}
+}
---
+++ src/main/java/com/demo/exception/ErrorDetails.java
@@ -0,0 +1,9 @@
+package com.demo.exception;
+
+import lombok.Data;
+
+@Data
+public class ErrorDetails {
+	
+	private String message;
+}
